d81d6a38b972e59460cbb3831ee82f711e6995fd
nohaemad123 <noha2697@gmail.com>
2024-08-12 15:04:40 +0300

    first commit

---
+++ catalogs.php
@@ -0,0 +1,28 @@
+<div class="heading text-center">
+        <div class="overlay">
+            <h3>Catalogs</h3>
+            <p><a href="index.html">Home</a> > Catalogs</p>
+        </div>
+    </div>
+        
+        <div class="about">
+         <div class="container">
+            <div class="title-main-w3ls text-center">
+                <h3 class="title-w3-agile"><span>C</span>atalogs</h3>
+                <div class="line-w3ls">
+                    <span class="fa fa-wheelchair"></span>
+                </div>
+            </div>
+             <div class="row">
+             <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                 <div class="catalog text-center">
+                     <div class="icon">
+                 <i class="fa fa-file-pdf-o"></i>
+                     </div>
+                     <h3>Catalog 1</h3>
+                     <a href="" download=""><button>Download</button></a>
+                 </div>
+                 </div>
+             </div>
+            </div>
+        </div>
---
+++ js/plugin.js
@@ -0,0 +1,11 @@
+/*global $, alert, document, eq*/
+
+$(document).ready(function () {
+    'use strict';
+
+    $(' .carousel').carousel({
+     interval: 1000
+    });
+
+
+});
